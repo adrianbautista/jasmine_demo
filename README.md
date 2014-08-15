5fd9d17594117d57e2d6dca5a51f1f8dd0a4b79d
Adrian Bautista <adrianbautista8@gmail.com>
2014-08-14 22:22:25 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+This is a demo using Jasmine for JavaScript testing. Don't forget your semicolons!
+
+[**Notes from the lesson**](https://gist.github.com/adrianbautista/3cd01aee457fd2791972)
+
+===
+
+## Getting Started
+
+#### Prereqs
+- Ruby 2.1.0
+- Rails 4.0.4
+
+1. Clone this repo
+2. `bundle install`
+3. `cp config/database.yml.example config/database.yml`
+4. Update `database.yml` with your own database credentials
+5. `rails s`
+6. Profit.
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JasmineDemo::Application.config.secret_key_base = 'f31684c514cd36fd918ffc9e0a2533032bce07f05c1da3a9faeb19ae6bf8b41c03f9df87d9c6d0df185103ca1e5aaeb0613805c16248e2ab19ef334e526db20f'
